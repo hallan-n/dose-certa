2619b671ae3522cc29089e77af35ff0c03ad64d9
Hállan Neves <hallanneves01@gmail.com>
2024-05-22 21:54:00 -0300

    Header shared (#3)
    * teste
    
    * feat: add header shared in all pages

---
+++ app/assets/shared/header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../css/global.css">
+<header class="p-4" style="background-color: rgb(5,80,156);">
+    <div class="d-flex justify-content-between mx-auto px-3" style="max-width: 900px;">
+        <a href="/pages/medication_list.php">
+            <img src="../assets/images/logo.png" alt="logo" width="100" height="100%">
+        </a>        
+        <span class="fs-1 text-light material-symbols-outlined">account_circle</span>
+    </div>
+</header>
