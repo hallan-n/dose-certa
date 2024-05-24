6d3c9e23b04cd90356805d67f08639d67ad4b591
hallan-n <hallanneves01@gmail.com>
2024-05-24 02:32:38 -0300

    refactor: modify footer

--- app/assets/shared/footer.php
+++ app/assets/shared/footer.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../css/global.css">
-<footer class="bg-dark p-4 text-light fixed-bottom">
+<footer class="bg-dark p-4 text-light fixed-bottom" id="footer-global">
     <div class="d-flex align-items-center justify-content-between mx-auto" style="max-width: 900px;">
         <p class="m-0">© Todos os Direitos Reservados<b> - Grupo 11 | 05/2024</b></p>
         <p class="m-0">Design by <b>Grupo 11</b></p>
