4500826f9ca688ec505e47f2b25ccdf03395b1b1
StanleyZ0 <jonathan.hy@hotmail.com>
2024-05-24 00:21:12 -0300

    refactor: user_password and user_profile action and button

--- app/pages/user_password.php
+++ app/pages/user_password.php
@@ -16,17 +16,17 @@
     <main class="px-5 d-flex align-items-center flex-column justify-content-center">
         <div class="w-100 d-flex" id="user_limited">
             <section class="w-100" id="user_img">
-                <img src="../assets/images/account-edit.png" alt="" width="100%" style="max-width: 502px;">
+                <img src="../assets/images/login.jpg" alt="" width="100%" style="max-width: 502px;">
             </section>
             <section class="w-100">
                 <form class="d-flex flex-column mx-auto" action="" method="post">
-                    <h1 class="text-nowrap fw-bold">Editar Perfil</h1>
-                    <label class="form-label mt-3" for="text">Nome</label>
-                    <input class="form-control mt-2 p-2" type="text" name="name" id="name">
-                    <label class="form-label mt-3" for="email">Email</label>
-                    <input class="form-control mt-2 p-2" type="email" name="email" id="email">
-                    <label class="form-label mt-3" for="tel">Telefone</label>
-                    <input class="form-control mt-2 p-2" type="tel" name="tel" id="tel">
+                    <h1 class="text-nowrap fw-bold">Editar senha</h1>
+                    <label class="form-label mt-3" for="previous">Senha antiga:</label>
+                    <input class="form-control mt-2 p-2" type="password" name="previous" id="previous">
+                    <label class="form-label mt-3" for="new_password">Senha nova:</label>
+                    <input class="form-control mt-2 p-2" type="password" name="new_password" id="new_password">
+                    <label class="form-label mt-3" for="confirm_password">Confirmação de senha:</label>
+                    <input class="form-control mt-2 p-2" type="password" name="confirm_password" id="confirm_password">
                     <div class="mt-3 d-flex gap-3 "> 
                         <button class="btn btn-primary w-100 p-2" type="submit">Salvar</button>
                         <a href="/" class="btn btn-secondary w-100  p-2" type="submit">Cancelar </a>
@@ -36,5 +36,4 @@
         </div>
     </main>
 </body>
-
 </html>
