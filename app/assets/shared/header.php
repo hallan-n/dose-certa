720fb6a1db2381a27ca838d9279a99f6e73d29f2
hallan-n <hallanneves01@gmail.com>
2024-05-31 12:58:33 -0300

    refactor: align items header

--- app/assets/shared/header.php
+++ app/assets/shared/header.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../assets/shared/header.css">
 
 <header class="p-4" style="background-color: rgb(5,80,156);">
-    <div class="d-flex justify-content-between mx-auto px-3" style="max-width: 900px;">
+    <div class="d-flex justify-content-between mx-auto px-3 align-items-center" style="max-width: 900px;">
         <a href="/pages/medication_list.php">
             <img src="../assets/images/logo.png" alt="logo" width="100" height="100%">
         </a>
