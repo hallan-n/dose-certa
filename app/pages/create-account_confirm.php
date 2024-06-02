13af348aed171b31c909725f564f8c4fe0398e6a
hallan-n <hallanneves01@gmail.com>
2024-06-02 20:49:50 -0300

    refactor: normalize confirm pages

--- app/pages/create-account_confirm.php
+++ app/pages/create-account_confirm.php
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <section>
                 <div class="d-flex flex-column">
                     <span style="font-size: 600%" class="mx-auto material-symbols-outlined">account_circle</span>
-                    <h1 class="text-nowrap fw-bold text-center">Confirmação de Cadastro</h1>
+                    <h1 class="fw-bold text-center">Confirmação de Cadastro</h1>
                 </div>
             </section>
             <section>
