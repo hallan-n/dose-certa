6da20878a92dc27e798bcf4a087c0c2480a59206
Hállan Neves <hallanneves01@gmail.com>
2024-06-02 20:43:32 -0300

    refactor: ref. buttons and header (#17)

--- app/pages/medication_list.php
+++ app/pages/medication_list.php
@@ -28,18 +28,57 @@
     <title>Lista de remédios</title>
 </head>
 <body>
-    <?php include "../assets/shared/header.php" ?>
+
+    <header id="fixed-header" class="p-4" style="background-color: rgb(5,80,156);">
+        <div class="d-flex justify-content-between mx-auto px-3 align-items-center" style="max-width: 900px;">
+            <a href="/pages/medication_list.php">
+                <img src="../assets/images/logo.png" alt="logo" width="100" height="100%">
+            </a>
+            <div>
+                <a href="/pages/user_profile.php">
+                    <span class="fs-1 me-4 text-light material-symbols-outlined button-header">account_circle</span>
+                </a>
+                <button type="button" data-bs-toggle="modal" data-bs-target="#modal-logout" style="all: unset; cursor: pointer;">
+                    <span class="fs-1 text-light material-symbols-outlined button-header">logout</span>
+                </button>
+            </div>
+        </div>
+    </header>
+
+
+    <!-- MODAL -->
+    <div class="modal fade" id="modal-logout" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">Tem certeza que deseja fazer logout?</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p><b>Atenção:</b> Sua sessão será encerra, será preciso fazer login novamente.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <a class="btn btn-primary" href="/pages/logout.php">Confirmar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- MODAL -->
+
+
     <main class="p-4 mx-auto" style="max-width: 900px;">
-        <div class="d-flex justify-content-between mt-4 mb-4">
-            <h1 class="text-nowrap fw-bold">Lista de remédios</h1>
-            <div id="add-div">
-                <div id="add">
-                    <span class="add-bt material-symbols-outlined p-2 me-2">notifications</span>
+        <div id="fixed-div" class="d-flex justify-content-between mt-4 gap-3 pe-5 pt-4">
+            <h1 class="fw-bold">Lista de remédios</h1>
+            <div>
+                <div class="d-flex flex-nowrap align-items-center gap-2">
+                    <span class="add-bt material-symbols-outlined p-2 me-2" style="width:40px; height:40px;">notifications</span>
                     <a href="/pages/medication_add.php" class="text-dark"><span class="add-bt material-symbols-outlined p-3 me-2">add</span></a>
                 </div>
             </div>
         </div>
-        <div id="medications" class="d-flex flex-wrap gap-2">
+        <div id="medications" class="d-flex flex-wrap gap-2" style="padding-top:200px;">
             <?php
                 if ($results != null) {
                     for ($i = 0; $i < count($results); $i++) {
